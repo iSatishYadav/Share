cdf8606c84ce322edb314d2556760b9dd5aaf21a
Satish Yadav <monstersatish@live.com>
2019-03-25 21:31:42 +0530

    QR rendering optimizations and refactor

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = "cache-v2";
+const CACHE_NAME = "cache-v3";
 const ASSET_TO_CACHE = [
-    "/index.html",
-    "/scripts/qrcode.min.js",
-    "/images/icons/icon-72x72.png",
-    "/images/icons/icon-96x96.png",
-    "/images/icons/icon-128x128.png",
-    "/images/icons/icon-144x144.png",
-    "/images/icons/icon-152x152.png",
-    "/images/icons/icon-192x192.png",
-    "/images/icons/icon-384x384.png",
-    "/images/icons/icon-512x512.png"
+    "index.html",
+    "scripts/qrcode.min.js",
+    "images/icons/icon-72x72.png",
+    "images/icons/icon-96x96.png",
+    "images/icons/icon-128x128.png",
+    "images/icons/icon-144x144.png",
+    "images/icons/icon-152x152.png",
+    "images/icons/icon-192x192.png",
+    "images/icons/icon-384x384.png",
+    "images/icons/icon-512x512.png"
 ];
 
 self.addEventListener('install', function (event) {
